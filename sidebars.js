46a3037094c3b38f257654e60d94047e4f3f125f
kumarsaurabh2727 <kumarsaurabh2727@gmail.com>
2026-08-26 13:56:30 +0530

    Initial Commit

---
+++ sidebars.js
@@ -0,0 +1,14 @@
+const sidebars = {
+  docsSidebar: [
+    'intro',
+    {
+      type: 'category',
+      label: 'Theme Module',
+      items: [
+        'theme-module/accessing-theme-module', 'theme-module/creating-new-theme', 'theme-module/color-and-typography'
+      ],
+    },
+  ],
+};
+
+export default sidebars;
