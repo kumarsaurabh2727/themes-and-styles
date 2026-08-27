dc85fa3e1389ff5e447b1a5c88b412e7dad2d23f
kumarsaurabh2727 <kumarsaurabh2727@gmail.com>
2026-08-27 16:23:17 +0530

    New Color submodule added

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,16 @@ const sidebars = {
       type: 'category',
       label: 'Theme Module',
       items: [
-        'theme-module/accessing-theme-module', 'theme-module/creating-new-theme', 'theme-module/color-and-typography'
+        'theme-module/accessing-theme-module',
+        'theme-module/color-and-typography',
+        'theme-module/creating-new-theme',
+        {
+          type: 'category',
+          label: 'Colors',
+          items: [
+            'theme-module/colors/introduction-to-colors',
+          ],
+        },
       ],
     },
   ],
